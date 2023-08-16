d6b5cdfc575b87d47cb3e2faa15c74685aefe503
tixvage <mel10h3421@gmail.com>
2023-08-16 17:31:12 +0300

    Added `raylib` as submodule

--- nobuild.c
+++ nobuild.c
@@ -1,41 +1,56 @@
 #define NOBUILD_IMPLEMENTATION
 #include "./nobuild.h"
 
-#define COMMON_CFLAGS "-Wall", "-Wextra", "-Werror", "-Wno-sign-compare", "-pedantic", "-std=c11", "-ggdb", "-O0", "-I."
+#define COMMON_CFLAGS "-Wall", "-Wextra", "-Werror", "-Wno-sign-compare", "-pedantic", "-std=c11", "-ggdb", "-O0", "-I.", "-Ilibs/raylib/src"
 #define CC "clang"
 #define SRCS "main.c", "buffer.c", "common.c"
 
+const char *program = NULL;
+
+void usage(void) {
+    INFO("Usage: %s [<subcommand>]", program);
+    INFO("Subcommands:");
+    INFO("    build");
+    INFO("        Build program");
+    INFO("    build-raylib");
+    INFO("        Build raylib");
+}
+
 void build(void) {
+    if (!path_exists("libs/raylib/src/libraylib.a")) {
+        usage();
+        PANIC("First of all go and build raylib");
+    }
     MKDIRS("build");
-    CMD(CC, COMMON_CFLAGS, "-o", "./build/uno", SRCS, "-lm", "-lraylib");
+    CMD(CC, COMMON_CFLAGS, "-o", "./build/uno", SRCS, "-lm", "-Llibs/raylib/src/", "-lraylib");
+}
+
+void build_raylib(void) {
+    MKDIRS("build");
+    CMD("cd", "libs/raylib/src");
+    CMD("make", "-C", "libs/raylib/src");
 }
 
 void run(void) {
     CMD("./build/uno");
 }
 
-void usage(const char *program) {
-    INFO("Usage: %s [<subcommand>]", program);
-    INFO("Subcommands:");
-    INFO("    build");
-    INFO("        Build program");
-    INFO("    run");
-    INFO("        Run program");
-}
-
 int main(int argc, char **argv) {
     GO_REBUILD_URSELF(argc, argv);
 
-    const char *program = shift_args(&argc, &argv);
+    program = shift_args(&argc, &argv);
     if (argc > 0) {
         const char *subcmd = shift_args(&argc, &argv);
         if (strcmp(subcmd, "build") == 0) {
             build();
+        } else if (strcmp(subcmd, "build-raylib") == 0) {
+            build_raylib();
         } else {
-            usage(program);
+            usage();
             PANIC("Unknown command `%s`", subcmd);
         }
     } else {
+        build_raylib();
         build();
         run();
     }
